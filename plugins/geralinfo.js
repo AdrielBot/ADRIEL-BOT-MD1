c260d0c96475950384df1d3a2e24acccbc2a6a23
ADRIEL <151471192+AdrielBot@users.noreply.github.com>
2023-11-21 21:48:28 -0400

    Actualizar geralinfo.js

--- plugins/geralinfo.js
+++ plugins/geralinfo.js
@@ -20,28 +20,47 @@ let readMore = more.repeat(850)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let str = `╭「𝙿𝚁𝙴𝙲𝙸𝙾𝚂 𝙳𝙴 𝙰𝙳𝚁𝙸𝙴𝙻-𝙱𝙾𝚃」
 │🐻‍❄️ *𝙷𝙾𝙻𝙰 𝚀𝚄𝙴 𝚃𝙰𝙻-${taguser}
-│🐻‍❄️ **𝙴𝚂𝚃𝙴 𝙴𝚂 𝙼𝙸 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙲𝙾𝙽𝚂𝚄𝙻𝚃𝙰𝚂:51935763050*
+│🐻‍❄️ **𝙴𝚂𝚃𝙴 𝙴𝚂 𝙼𝙸 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙲𝙾𝙽𝚂𝚄𝙻𝚃𝙰𝚂:+59168683798*
 │🐻‍❄️ *𝚃𝙴𝙽𝙶𝙾 𝙳𝙸𝙵𝙴𝚁𝙴𝙽𝚃𝙴𝚂 𝙿𝚁𝙴𝙲𝙸𝙾𝚂 𝙿𝙰𝚁𝙰 𝙼𝙸 𝙱𝙾𝚃𝙲𝙸𝚃𝙾
 ︎╰───────────────╯
 🍁᭢━━━━━𝙰𝙳𝚁𝙸𝙴𝙻-𝙱𝙾𝚃━━━━᭥🍁᭢
-╭「𝐏𝐀𝐐𝐔𝐄𝐓𝐄 𝐃𝐄 𝐁𝐎𝐓𝐒 𝐏𝐑𝐎𝐆𝐑𝐀𝐌𝐀𝐃𝐎𝐑」
-│-𝙿𝙰𝚁𝙰 𝙴𝚂𝚃𝙴 𝙱𝙾𝚃 𝚃𝙴𝙽𝙶𝙾 𝙴𝙻 𝙱𝙾𝚃 𝙿𝙴𝚁𝙼𝙰𝙽𝙴𝙽𝚃𝙴- 𝙱𝙾𝚃 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙾- 𝙱𝙾𝚃 𝙿𝚁𝙾𝙿𝙸𝙾𝚂
-│😼𝙱𝙾𝚃 𝙿𝙴𝚁𝙼𝙰𝙽𝙴𝙽𝚃𝙴:  Este bot lleva una personalizacion del creador es permanente y no tiene limite de grupos y te enseño a programarlo y aque tu mismo lo pongas en un numero
-│😼𝙱𝙾𝚃 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙾:  Este vendria ser un bot permanente de igual manera pero con lleva una personalizacion DE TU CLAN O TEAM O COMUNIDAD O E-SPORT Sin limite de grupos
-│😼𝙱𝙾𝚃 𝙿𝚁𝙾𝙿𝙸𝙾:  Este es un bot con permiso de venta aclaro que este bot es el unico que puede ser vendido y lleva la personalizacion que tu quieras mas veloz y sin limite de grupos
+╭「𝐏𝐑𝐄𝐂𝐈𝐎𝐒 𝐃𝐄 𝐁𝐎𝐓𝐂𝐈𝐓𝐎𝐒 𝐏𝐀𝐑𝐀 𝐆𝐑𝐔𝐏𝐎𝐒」
+│*💌BOT MENSUAL: 2 GRUPOS*
+│
+│🇵🇪10 SOLES
+│🇦🇷1000 PESOS
+│🇧🇴YAPE O TIGO 20 BS
+│💎 300 DIAMANTES
+│
+│*💌BOT TRIMESTRAL: 3 GRUPOS*
+│
+│🇵🇪15 SOLES
+│🇦🇷 1500 PESOS
+│🇧🇴YAPE O TIGO 36 BS  
+│💎 500 DIAMANTES 
+│
+│*💌BOT PERMANENTE:4 GRUPOS*
+│ 
+│🇵🇪19 SOLES
+│🇦🇷1,805 PESOS
+│🇧🇴Tigo 35 BS
+│_💎800 DIAMANTES_
+│
+│*💌BOT PERMANENTE PLUS:6 GRUPOS*
+│
+│_🇵🇪23 SOLES_
+│_🇦🇷2300 PESOS_
+│_🇧🇴Tigo 42 BS_
+│_💎800 DIAMANTES_
+│
+│𝐀𝐝𝐫𝐢𝐞𝐥𝐁𝐨𝐭
 ╰───────────────╯
-╭「𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍 𝐃𝐄 𝐁𝐎𝐓𝐂𝐈𝐓𝐎𝐒 𝐏𝐀𝐑𝐀 𝐆𝐑𝐔𝐏𝐎𝐒」
-│💌BOT MENSUAL : Este bot es unido a 3 grupos mediante a link por todo 1 MES
-│💌BOT TRIMESTRAL : Este Bot es unido a 4 grupos mediante a link por 3 MESES
-│💌BOT PERMANENTE : Bot unido a 3 grupos mediante a links de forma permanente(No me hago responsable si sacan al bot del grupo)
-│💌BOT PERMANENTE PLUS :Bot unido a 5 grupos mediante a links de forma permanente(No me hago responsable si sacan al bot del grupo)
-╰───────────────╯
+🐻‍❄️᭢━━━━━━━━━᭥🐻‍❄️᭢
 ╭「𝐀𝐂𝐋𝐀𝐑𝐀𝐂𝐈𝐎𝐍𝐄𝐒」
-│🤖NO HAGO VIDA SOCIAL AL PRIVAD
-│🤖NO HAGO REMBOLSOS SI NO ESTAS SEGUR@ DE COMPRAR ALGO NO LO HAGAS
-│🤖NO INTERCAMBIO PAQUETE DE BOTS PROGRAMADOR POR PAQUETE DE BOTS UNIDOS POR LINKS
-│🤖NO REGALO NADA, SOLO HAGO DESCUENTOS ALOS QUE YA AN SIDO CLIENTES O SI COMPRAN MAS COSAS
-╭🤖EL BOT PERMANENTE - BOT PERSONALIZADO TIENE PROHIBIDA SU VENTA
+│❄️NO HAGO VIDA SOCIAL AL PRIVD
+│❄️NO HAGO REMBOLSOS SI NO ESTAS SEGUR@ DE COMPRAR ALGO NO LO HAGAS
+│❄️NO ACEPTO SPAM COMO PAGO
+│❄️NO REGALO NADA, SOLO HAGO DESCUENTOS ALOS QUE YA AN SIDO CLIENTES O SI COMPRAN MAS COSAS
 ╰───────────────╯`.trim()
 if (m.isGroup) {
 //await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
